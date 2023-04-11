a8a463f3c87c88555a0133a5c5ed199593c9ba88
Ket_ch0p <96146875+Tomatotech90@users.noreply.github.com>
2023-04-11 16:30:19 -0400

    Update modbus_server.py

--- modbus_server.py
+++ modbus_server.py
@@ -1,45 +1,33 @@
-from flask import Flask, request, jsonify, send_from_directory
-from pymodbus.client.sync import ModbusTcpClient
+from pyModbusTCP.server import ModbusServer
 
-app = Flask(__name__)
+server = ModbusServer(host="localhost", port=502)
 
-modbus_server_ip = "127.0.0.1"  # Change this to your Modbus server IP address
-modbus_server_port = 502  # Change this to your Modbus server port number
-client = ModbusTcpClient(modbus_server_ip, port=modbus_server_port)
+def add_water(tank_id):
+    # Add water to the specified tank
+    pass
 
+def discharge_water(tank_id):
+    # Discharge water from the specified tank
+    pass
 
-@app.route('/dashboard.html')
-def dashboard():
-    return send_from_directory('static', 'dashboard.html')
+def update_percentage(tank_id, percentage):
+    # Update the water level of the specified tank
+    pass
 
+def add_to_log(message, type):
+    # Add the message to the log
+    pass
 
-@app.route('/api/tank_levels', methods=['GET'])
-def get_tank_levels():
-    response = client.read_holding_registers(0, 6)
-    if response.isError():
-        return jsonify(error=str(response)), 500
-    return jsonify(tank_levels=response.registers)
+def read_registers_callback(request):
+    # Callback function to handle incoming read register requests
+    # Return the water level of the specified tank
+    pass
 
+def write_registers_callback(request):
+    # Callback function to handle incoming write register requests
+    # Update the water level of the specified tank
+    pass
 
-@app.route('/api/add_water', methods=['POST'])
-def add_water():
-    tank_id = int(request.form.get('tank_id'))
-    amount = int(request.form.get('amount'))
-    current_level = client.read_holding_registers(tank_id, 1).registers[0]
-    new_level = min(current_level + amount, 10)
-    client.write_register(tank_id, new_level)
-    return jsonify(success=True)
-
-
-@app.route('/api/discharge_water', methods=['POST'])
-def discharge_water():
-    tank_id = int(request.form.get('tank_id'))
-    amount = int(request.form.get('amount'))
-    current_level = client.read_holding_registers(tank_id, 1).registers[0]
-    new_level = max(current_level - amount, 0)
-    client.write_register(tank_id, new_level)
-    return jsonify(success=True)
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)  # Change this to your desired IP address and port number
+server.start()
+server.add_read_callback(slave_id=1, function_code=3, callback=read_registers_callback)
+server.add_write_callback(slave_id=1, function_code=6, callback=write_registers_callback)
